d0a3268e1dc318bfcd31680eb07482171722e7e2
Petrus van Egeraat <pjj.vanegeraat@gmail.com>
2023-04-28 21:40:24 +0400

    lean & mean

---
+++ ios/nce-dev/NotificationViewController.h
@@ -0,0 +1,12 @@
+//
+//  NotificationViewController.h
+//  nce-dev
+//
+//  Created by Petrus van Egeraat on 4/28/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ ios/nce-dev/NotificationViewController.m
@@ -0,0 +1,29 @@
+//
+//  NotificationViewController.m
+//  nce-dev
+//
+//  Created by Petrus van Egeraat on 4/28/23.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *label;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.label.text = notification.request.content.body;
+}
+
+@end
