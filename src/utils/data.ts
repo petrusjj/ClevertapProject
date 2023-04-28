9635fdf29a276a793a1f949b663d04d729a1cb8b
Petrus van Egeraat <pjj.vanegeraat@gmail.com>
2023-04-28 12:59:28 +0400

    remove

--- src/utils/data.ts
+++
@@ -1,9 +0,0 @@
-export const petrus = {
-  Identity: 'e6c8ac1d-b830-4469-9596-bba81540513f',
-  Phone: '+971544965771',
-};
-
-export const lucas = {
-  Identity: '23e5437c-f46c-4835-bfc1-0ee9c733e28b',
-  Phone: '+971123456789',
-};
